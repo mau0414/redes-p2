2cf1fa1b264f3b689925ab6b4e4c1506de647899
Mauricio Almeida <galmeidamauricio12@gmail.com>
2024-04-26 21:38:31 -0300

    passos 1, 2 e 3

--- tcp.py
+++ tcp.py
@@ -1,5 +1,6 @@
 import asyncio
-from tcputils import *
+from grader.tcputils import *
+import random
 
 
 class Servidor:
@@ -17,6 +18,7 @@ class Servidor:
         """
         self.callback = callback
 
+    # chamada quando um segmento chega do lado do servidor (header|data)
     def _rdt_rcv(self, src_addr, dst_addr, segment):
         src_port, dst_port, seq_no, ack_no, \
             flags, window_size, checksum, urg_ptr = read_header(segment)
@@ -31,14 +33,18 @@ class Servidor:
         payload = segment[4*(flags>>12):]
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
+        # flag de SYN indica nova conexao a ser realizada
         if (flags & FLAGS_SYN) == FLAGS_SYN:
-            # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
+
+            # criacao de nova conexao - seq_no + 1 eh o ack para confirmar que recebeu pacote seq_no
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no, seq_no+1)
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
+
             if self.callback:
                 self.callback(conexao)
+
         elif id_conexao in self.conexoes:
             # Passa para a conexão adequada se ela já estiver estabelecida
             self.conexoes[id_conexao]._rdt_rcv(seq_no, ack_no, flags, payload)
@@ -48,22 +54,53 @@ class Servidor:
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, seq_no, ack_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
         self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
         #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
 
+        src_addr, src_port, dst_addr, dst_port = id_conexao
+
+        # criacao de header invertendo origem e destino e passando seq_no_y e ack de recebimento de seq_no
+        header = make_header(dst_port, src_port, seq_no, seq_no+1, FLAGS_ACK | FLAGS_SYN)
+
+        # arruma checksum antes do envio
+        header = fix_checksum(header, dst_addr, src_addr)
+
+        # envio do segmento para scr_addr (endereco de onde conexao veio)
+        self.servidor.rede.enviar(header, src_addr)
+
+        self.seq_no_to_send = seq_no + 1 # proximo seq_no a enviar (seq_no acabou de ser enviado)
+        self.seq_no_to_receive = ack_no # prox seq_no a receber
+
     def _exemplo_timer(self):
         # Esta função é só um exemplo e pode ser removida
         print('Este é um exemplo de como fazer um timer')
 
+    # funcao que recebe pacotes da conexao ja estabelecida
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
         # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
-        print('recebido payload: %r' % payload)
+        # print('recebido payload: %r' % payload)
+
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+
+        # garante que pacote que chegou eh o esperado e na ordem
+        # len(payload) > 0 garante que recebimento de pacote ack nao gera resposta
+        if (seq_no == self.seq_no_to_receive and len(payload) > 0):
+            
+            # incrementa proximo seq_no esperado 
+            self.seq_no_to_receive += len(payload)
+            self.callback(self, payload)
+
+            # envia segmento
+            segmento = make_header(dst_port, src_port, self.seq_no_to_send, self.seq_no_to_receive, FLAGS_ACK)
+            segmento = fix_checksum(segmento, dst_addr, src_addr)
+
+            self.servidor.rede.enviar(segmento, src_addr)
 
     # Os métodos abaixo fazem parte da API
 
@@ -74,6 +111,12 @@ class Conexao:
         """
         self.callback = callback
 
+    # Nesta primeira parte, vamos nos preocupar somente com o evento "dados recebidos da camada de aplicação acima". Ou seja, 
+    # mantenha uma contagem correta do número de sequência a ser inserido no segmento e construa o segmento corretamente, 
+    # enviando-o em seguida para a camada de rede.
+    # Por simplicidade, mantenha a flag de ACK sempre ligada nos segmentos que você enviar, incluindo corretamente o acknowledgement 
+    # number correspondente ao próximo byte que você espera receber. Note que essa contagem refere-se ao funcionamento da nossa ponta
+    # como receptor, e não como transmissor, mas aqui misturamos os papéis.
     def enviar(self, dados):
         """
         Usado pela camada de aplicação para enviar dados
@@ -81,7 +124,23 @@ class Conexao:
         # TODO: implemente aqui o envio de dados.
         # Chame self.servidor.rede.enviar(segmento, dest_addr) para enviar o segmento
         # que você construir para a camada de rede.
-        pass
+        src_addr, src_port, dst_addr, dst_port = self.id_conexao
+
+        while len(dados) > 0:
+
+            # limita payload a MSS (Maximum Segment Size) para evitar flow problems
+            payload = dados[:MSS]
+
+            # eventuais dados que sobraram
+            dados = dados[MSS:]
+
+            segmento = make_header(dst_port, src_port, self.seq_no_to_send, self.seq_no_to_receive, FLAGS_ACK)
+            segmento = fix_checksum(segmento+payload, dst_addr, src_addr)
+            self.servidor.rede.enviar(segmento, src_addr)
+
+            # atualiza proximo seq_no a enviar
+            self.seq_no_to_send += len(payload)
+
 
     def fechar(self):
         """
